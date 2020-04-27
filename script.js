a174ac7303140927a043a39d0d12eee397719293
Szofi <szofinauta@gmail.com>
2020-04-28 00:33:55 +0200

    Refactor images

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('hello!');
+const images = [
+	{
+		title: "Night scene",
+		source: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/fdcdde66672659.5b92e224c0fc6.jpg",
+		description: "bird-eye view of a church and pine trees at night"
+	},
+
+	{
+		title: "Vigilant owl",
+		source: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c1665651777425.58f94d2b5b72c.jpg",
+		description: "dark blue night scenery with staring red-eyed owl"
+	},
+
+	{
+		title: "Foggy house",
+		source: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/4d4cbf51777425.58f94d2b5cd1e.jpg",
+		description: "night scenery of house in fog with purple and blue hues"
+	},
+
+	{
+		title: "Neon Seagulls",
+		source: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/aa4a2a63573187.5b7e0f3ddce5e.jpg",
+		description: "night scenery of street reflecting neon pink harsh artificial light after rain with flying seagulls"
+	}
+];
